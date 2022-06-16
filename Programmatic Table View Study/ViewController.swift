5458734f7283749f30557c4c8d17c00f3153598d
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-06-16 13:01:53 +0300

    Initial Commit

---
+++ Programmatic Table View Study/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Programmatic Table View Study
+//
+//  Created by Ömer Faruk Kılıçaslan on 16.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
